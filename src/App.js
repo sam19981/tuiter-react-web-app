c7c3b17e157f3be2f2808876a2726d4bf9906e35
sankar <rsankar19981@gmail.com>
2023-06-06 18:58:18 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import Labs from "./labs";
 import HelloWorld from "./labs/a3/hello-world";
 import Tuiter from "./tuiter";
-import {BrowserRouter, Navigate} from "react-router-dom";
+import {BrowserRouter, Navigate,Link} from "react-router-dom";
 import {Routes, Route} from "react-router";
+import Asd from "./Asd"
+import Rew from "./Rew"
 
 function App() {
-  return (
-   <BrowserRouter>
-   <div className="container">
-     <Routes>
-       <Route path="/"         element={<Navigate to="/labs"/>}/>
-       <Route path="/labs/*"   element={<Labs/>}/>
-       <Route path="/hello"    element={<HelloWorld/>}/>
-       <Route path="/tuiter/*" element={<Tuiter/>}/>
-     </Routes>
-   </div>
- </BrowserRouter>
- 
- 
-  );
+    return(
+        <BrowserRouter>
+            <Link to="/qwe/21/14/wer">LINK A</Link>
+            <Routes>
+                <Route path="/qwe/:qwe/:wer/wer" element={<Rew/>}/>
+                <Route path="/asd/:zxc/:xcv" element={<Asd/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 export default App;
 
